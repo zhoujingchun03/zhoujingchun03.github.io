81ac8bbe9925e84458aec692ddf9c7db10872961
Zhoujingchun03 <630790387@qq.com>
2023-12-04 12:23:57 +0800

    update students

--- site.js
+++ site.js
@@ -131,7 +131,7 @@ $.getJSON('/graduates.json', function (data) {
         let el = $('#graduates-table')
         let str = s
 
-        el.html(el.html() + `<tr>${s}</tr>`)
+        el.html(el.html() + `<p>${str}</p>`)
         for (let i = 0; i < (l.length + 3) / 4; i++) {
             let innerHtml = '';
 
